0d164b4d3fe63ee41b77f8f418c0e9ba4e7c1ac5
AndroidStorm <williamg.dev@gmail.com>
2018-05-02 22:53:38 -0700

    Display the first Name always no matter if the activity is recreated again

--- app/src/main/java/com/github/example/newandroidcomponents/HelloViewModel.java
+++ app/src/main/java/com/github/example/newandroidcomponents/HelloViewModel.java
@@ -3,4 +3,13 @@ package com.github.example.newandroidcomponents;
 import android.arch.lifecycle.ViewModel;
 
 class HelloViewModel extends ViewModel{
+    private String message;
+
+    public void sayHello(String message) {
+        this.message = "Hello " + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
+++ app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
@@ -3,14 +3,34 @@ package com.github.example.newandroidcomponents;
 import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     HelloViewModel helloViewModel;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         helloViewModel = ViewModelProviders.of(this).get(HelloViewModel.class);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void saySomething(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                if (helloViewModel.getMessage() == null) {
+                    helloViewModel.sayHello(editText.getText().toString());
+                }
+                showToast(helloViewModel.getMessage());
+                break;
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }

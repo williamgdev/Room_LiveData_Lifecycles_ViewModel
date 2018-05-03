c3c7736a1be4f3f3574fd93e48df52e8c4428cf2
AndroidStorm <williamg.dev@gmail.com>
2018-05-02 22:38:47 -0700

    Created ViewModel and Handled Lifecycles

---
+++ app/src/main/java/com/github/example/newandroidcomponents/HelloViewModel.java
@@ -0,0 +1,6 @@
+package com.github.example.newandroidcomponents;
+
+import android.arch.lifecycle.ViewModel;
+
+class HelloViewModel extends ViewModel{
+}
--- app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
+++ app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
@@ -1,13 +1,16 @@
 package com.github.example.newandroidcomponents;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    HelloViewModel helloViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloViewModel = ViewModelProviders.of(this).get(HelloViewModel.class);
     }
 }

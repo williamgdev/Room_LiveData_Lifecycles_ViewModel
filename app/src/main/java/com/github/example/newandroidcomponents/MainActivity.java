176bc822cb09074528d45a5cacdb9010482e780d
Will Gutierrez <williamg.dev@gmail.com>
2018-06-20 16:03:03 -0700

    DataBinding MainActivity

--- app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
+++ app/src/main/java/com/github/example/newandroidcomponents/MainActivity.java
@@ -2,29 +2,30 @@ package com.github.example.newandroidcomponents;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import com.github.example.newandroidcomponents.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getCanonicalName();
     HelloViewModel helloViewModel;
-    private EditText editText;
     TimerInteractor interactor;
     private Example1Fragment example1Fragment;
     private Example2Fragment example2Fragment;
     private TimeViewModel timeViewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         helloViewModel = ViewModelProviders.of(this).get(HelloViewModel.class);
         timeViewModel = ViewModelProviders.of(this).get(TimeViewModel.class);
-        editText = findViewById(R.id.editText);
         subscribe();
         int number = timeViewModel.getNumber().getValue() == null ? 0 : timeViewModel.getNumber().getValue();
         interactor = new TimerInteractor(this, number++, new TimerInteractor.TimerInteractorListener() {
@@ -67,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
     public void saySomething(View view) {
         switch (view.getId()) {
             case R.id.button:
-                helloViewModel.sayHello(editText.getText().toString());
+                helloViewModel.sayHello(binding.editText.getText().toString());
                 break;
         }
     }
